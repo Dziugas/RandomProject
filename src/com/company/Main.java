5ab90378c8fcd6bc1748c66b4218c0b551e0202d
Dziugas <dziugas@graphity.org>
2017-04-05 13:48:27 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        System.out.println("Labas. Išmesime tau random skaičius nuo x iki y.");
+        System.out.println("Įvesk skaičių x");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("Įvesk skaičių y");
+        int y = sc.nextInt();
+        System.out.println("Kiek skaičių reikės?");
+        int z = sc.nextInt();
+        System.out.println("Išmesime "+(z)+" atsitiktinių skaičių nuo "+(x)+" iki "+(y));
+
+        for (int i = 0; i <z ; i++) {
+            Random rand = new Random();
+            int n = rand.nextInt(y)+x;
+            System.out.println(n);
+        }
+
+
+
+
+
+
+    }
+}
