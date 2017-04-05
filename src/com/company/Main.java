0548dc8fc690e66fc40e1ca6c2ec7ca8ebfb087a
Dziugas <dziugas@graphity.org>
2017-04-05 13:54:56 +0300

    fixed y number (it is not the max, it is the distance from x)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,21 +12,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         System.out.println("Įvesk skaičių y");
-        int y = sc.nextInt();
+        int y = sc.nextInt()-x;
         System.out.println("Kiek skaičių reikės?");
         int z = sc.nextInt();
-        System.out.println("Išmesime "+(z)+" atsitiktinių skaičių nuo "+(x)+" iki "+(y));
+        System.out.println("Išmesime " + (z) + " atsitiktinių skaičių nuo " + (x) + " iki " + (y));
 
-        for (int i = 0; i <z ; i++) {
+        for (int i = 0; i < z; i++) {
             Random rand = new Random();
-            int n = rand.nextInt(y)+x;
+            int n = rand.nextInt(y) + x;
             System.out.println(n);
         }
-
-
-
-
-
-
     }
 }
